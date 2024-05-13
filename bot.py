3082c5fefc6cadcd1421d77ea7c0e0eb9267a874
COOL_TECH <156781377+Jeronmathew44@users.noreply.github.com>
2024-05-13 12:46:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 from pyrogram import Client, filters
 
 # Replace "your_api_id", "your_api_hash", and "your_bot_token" with your actual credentials
-api_id = "27215224"
-api_hash = "688ae67db37f0ae991c3ecb97d73ff0a"
-bot_token = "6719248453:AAFhRI3RkY-76lYUDxCob18OLMF0XgkMD4I"
+api_id = "Ypur_api_id"
+api_hash = "You_api_hash"
+bot_token = "Your_bot_token"
 
 # Create a Pyrogram Client
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
