fd39c65d450a76f4219762147f86e3c1cfa29d29
COOL_TECH <156781377+Jeronmathew44@users.noreply.github.com>
2024-05-18 15:40:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,26 @@
+import os
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+# Fetch sensitive data from environment variables
+API_ID = os.getenv("API_ID")
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-# Replace "your_api_id", "your_api_hash", and "your_bot_token" with your actual credentials
-api_id = "Ypur_api_id"
-api_hash = "You_api_hash"
-bot_token = "Your_bot_token"
-
-# Create a Pyrogram Client
-bot = Client("countdown_bot")
+bot = Client(
+    "countdown_bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
 
 # Define a command handler for the /start command
 @bot.on_message(filters.command("start"))
 async def start_command(client, message):
-    welcome_message = "Welcome to the Countdown Timer Bot!\nSend me the time in seconds, minutes, or hours to start the countdown."
+    welcome_message = (
+        "Welcome to the Countdown Timer Bot!\n"
+        "Send me the time in seconds, minutes, or hours to start the countdown."
+    )
 
     # Create InlineKeyboardButtons for joining the support group
     support_button = InlineKeyboardButton("Join Support Group", url="https://t.me/XBOTSUPPORTS")
@@ -38,7 +45,6 @@ async def help_command(client, message):
     keyboard = InlineKeyboardMarkup([[usage_button], [support_button]])
 
     await message.reply_text(help_message, reply_markup=keyboard)
-
 
 # Run the bot
 bot.run()
